30e4309f1f373b4ec5a34fea8afa2490bf0325d1
Duy-Hieu Bui <duyhieu.bui@gmail.com>
2026-02-07 22:09:41 -0500

    Add build scripts and meta file for OpenRoad

---
+++ recipes/openroad/build.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+# Identify OS
+UNAME_OUT="$(uname -s)"
+case "${UNAME_OUT}" in
+    Linux*)     OS=Linux;;
+    Darwin*)    OS=Mac;;
+    *)          OS="${UNAME_OUT}"
+                echo "Unknown OS: ${OS}"
+                exit;;
+esac
+
+if [[ $OS == "Mac" ]]; then
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+    cd $PREFIX
+    # This is required because we have an external SDK file. The default
+    # Qt build explicitly looks for the SDK in /System/Library/Frameworks/
+    # and ignores any attempt to set the SDK root any other way.
+    if grep -q _GL_INCDIRS "$PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake"; then
+        sed '1,11d' < "$PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake" | sed '1 s|.*|set(_qt5gui_OPENGL_INCLUDE_DIR "'$CONDA_BUILD_SYSROOT'/System/Library/Frameworks/OpenGL.framework/Headers")|' > "$PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake.bak"
+        mv "$PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake.bak" "$PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake"
+    fi
+fi
+
+which cmake
+cd $SRC_DIR/third_party/lemon
+$BUILD_PREFIX/bin/cmake -B build  -DCMAKE_INSTALL_PREFIX=$PREFIX 
+$BUILD_PREFIX/bin/cmake --build build -j $CPU_COUNT --target install
+
+cd $SRC_DIR/third_party/cudd
+./configure --prefix="${PREFIX}"
+make V=1 -j$CPU_COUNT
+make V=1 install
+
+cd $SRC_DIR/third_party/or-tools
+$BUILD_PREFIX/bin/cmake -B build  -DBUILD_DEPS:BOOL=ON \
+      -DBUILD_EXAMPLES:BOOL=OFF \
+      -DBUILD_SAMPLES:BOOL=OFF \
+      -DBUILD_TESTING:BOOL=OFF  \
+      -DCMAKE_CXX_FLAGS="-w" \
+      -DCMAKE_C_FLAGS="-w" \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      .
+$BUILD_PREFIX/bin/cmake --build build --config Release -j $CPU_COUNT --target install
+
+## there is a bug with SPDLOG 1.11 and fmt 0.9
+## disable SPDLOG_FMT_EXTERNAL
+cd $SRC_DIR
+$BUILD_PREFIX/bin/cmake -B build -DTCL_LIB_PATHS="$BUILD_PREFIX;$PREFIX" \
+      -DCMAKE_FIND_ROOT_PATH="$BUILD_PREFIX;$PREFIX" \
+      -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY \
+      -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
+      -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=ONLY \
+      -DUSE_SYSTEM_BOOST=ON \
+      -DSPDLOG_FMT_EXTERNAL=OFF \
+      -DCMAKE_CXX_FLAGS="-I$BUILD_PREFIX/include -I$PREFIX/include -L$BUILD_PREFIX/lib -L$PREFIX/lib -lxcb-glx -lxcb-dri2" \
+      -DCMAKE_C_FLAGS="-I$BUILD_PREFIX/include -I$PREFIX/include -L$BUILD_PREFIX/lib -L$PREFIX/lib -lxcb-glx -lxcb-dri2" \
+      -DCMAKE_EXE_LINKER_FLAGS="-L$BUILD_PREFIX/lib -L$PREFIX/lib -lxcb-glx -lxcb-dri2" \
+      -DENABLE_TESTS=OFF -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DFLEX_INCLUDE_DIR=$BUILD_PREFIX/include \
+      .
+
+$BUILD_PREFIX/bin/cmake --build build -j $CPU_COUNT --target install
